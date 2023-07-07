247a86109c4a4104bd5fb283d86853772adb8d97
Srushti Shete <shetesrushti11@gmail.com>
2023-07-08 00:07:42 +0530

    Designed & Completed Home Page Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import "./home.scss"
+import Stories from "../../components/stories/Stories"
+import Posts from "../../components/posts/Posts"
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="home">
+      <Stories/>
+      <Posts/>
     </div>
   )
 }
