bf31e88240ecc313c5f1171a9529db1a61ae47ab
Srushti Shete <shetesrushti11@gmail.com>
2023-07-07 13:38:41 +0530

    Designed and Completed LeftBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import LeftBar from "./components/leftBar/LeftBar";
+import Navbar from "./components/navbar/Navbar";
+import RightBar from "./components/rightBar/RightBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
+const currentUser = true;
+
+  const Layout = () => {
+
+    return(
+      <div>
+        <Navbar/>
+        <div style={{display: "flex"}}>
+          <LeftBar/>
+          <div style={{ flex:6 }}>
+          <Outlet/>
+          </div>
+          <RightBar/>
+        </div>
+      </div>
+    )
+  }
+const ProtectedRoute = ({children}) => {
+  if(!currentUser){
+    return <Navigate to = "/login"/>
+  }
+  return children 
+}
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+      <ProtectedRoute>
+        <Layout/>
+      </ProtectedRoute>
+      ),
+      children: [{
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile/>
+      }
+    ]
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router = {router} />
     </div>
   );
 }
